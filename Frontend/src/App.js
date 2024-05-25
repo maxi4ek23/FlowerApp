9f2b6bef14bcad2ef1205207fd1d33cc59670ff4
unknown <maxi4ek23@gmail.com>
2024-05-25 17:56:32 +0300

    created and styled Login Page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -5,6 +5,7 @@ import {Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
 import Catalog from './pages/Catalog';
 import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<Home />}></Route>
         <Route path='/catalog' element={<Catalog />}></Route>
         <Route path='/login' element={<Login />}></Route>
+        <Route path='/signup' element={<Signup />}></Route>
       </Routes>
     </BrowserRouter>
   );
--- Frontend/src/pages/Login.js
+++ Frontend/src/pages/Login.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import './Login.css';
+import Icon, {
+  UserOutlined,
+  LockOutlined
+} from "@ant-design/icons";
+import { NavLink } from 'react-router-dom';
 
-function Login () {
+function Login() {
+
+  const handle = () => {
+    console.log('11');
+  }
+
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className='login__page'>
+      <div className='login__form'>
+        <h1>Login</h1>
+        <form onSubmit={handle}>
+          <div className='input__block'>
+            <div className='input__field'>
+              <Icon className='icon' component={UserOutlined} />
+              <input type='text' placeholder='Email'></input>
+            </div>
+            <div className='input__field'>
+              <Icon className='icon' component={LockOutlined} />
+              <input type='password' placeholder='Password'></input>
+            </div>
+            <p>You don`t have an account? <NavLink to='/signup'>Sigh up</NavLink></p>
+          </div>
+          <button type='submit' className='input__btn'>Login</button>
+        </form>
+      </div>
+
     </div>
   );
 };
