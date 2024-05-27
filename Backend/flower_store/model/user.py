9803880525ad0acf77d72c2efa22670a497d423f
YuliaDyka <handsmade.yulia@gmail.com>
2024-05-27 09:48:43 +0300

    fixed some problems and changed card logic

--- Backend/flower_store/model/user.py
+++ Backend/flower_store/model/user.py
@@ -11,7 +11,8 @@ class User(db.Model, IDto):
     email: str = db.Column(db.String(45), nullable=False)
     password: str = db.Column(db.String(45), nullable=False)
     is_admin: bool = db.Column(db.Boolean, nullable=False)
-    bonus_card = db.relationship("BonusCard", backref="user")
+    card_type: str = db.Column(db.String(45))
+    # bonus_card = db.relationship("BonusCard", backref="user")
     # bonus_card_id: int = db.Column(db.Integer, db.ForeignKey("bonus_card"))
 
 
@@ -22,7 +23,7 @@ class User(db.Model, IDto):
             "email": self.email,
             "password": self.password,
             "is_admin": self.is_admin,
-            "bonus_card": self.bonus_card[0].put_into_dto() if len(self.bonus_card) >= 1 else {}
+            "bonus_card": self.card_type
         }
 
     @staticmethod
